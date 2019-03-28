7b4cf4ec08e352972155d8dfb157ce29e371d02b
antoinebaudoux <a.baudoux@gmail.com>
2019-03-28 20:17:57 +0100

    Initial structure

--- index.ts
+++ index.ts
@@ -1,6 +1,12 @@
 // Import stylesheets
-import './style.css';
+import './style.css'
 
 // Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+const appDiv: HTMLElement = document.getElementById('app')
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`
+
+const button: HTMLButtonElement = document.getElementById('scan') as HTMLButtonElement
+
+button.addEventListener('pointerup', function(event) {
+  // Call navigator.bluetooth.requestDevice
+});
